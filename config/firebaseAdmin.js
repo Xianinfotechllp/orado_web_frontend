39fab61f377fc17ee9c545cd851a64c7482c747a
amarnadh777 <amarnadh6565@gmail.com>
2025-05-27 11:35:40 +0530

    rm

--- config/firebaseAdmin.js
+++
@@ -1,9 +0,0 @@
-var admin = require("firebase-admin");
-
-
-var serviceAccount = require('./orado-1eb0c-firebase-adminsdk-fbsvc-700cb01560.json');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-module.exports = admin;
--- routes/locationRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {getRestaurantsInServiceArea} = require('../controllers/locationControllers')
-router.get("/nearby-restaurants",getRestaurantsInServiceArea)
-// router.get("/nearby-categories")
-module.exports = router;
