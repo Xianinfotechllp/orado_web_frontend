4bbc6857d43db886625db0c83d34ea7a535578e5
Aravind-R-Warrier <aravindrwarrier1@gmail.com>
2025-05-27 16:24:45 +0530

    -nav-header-scroll

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -20,44 +20,58 @@ function Home() {
   return (
     <div>
       <Navbar />
-         {/* homeBanner */}
-      <div className="flex flex-row ">
-        <div className="w-full  mb-10 md:mb-0">
-          <h1 className="text-4xl font-bold text-gray-800 leading-tight">
+      {/* homeBanner */}
+      <div className="flex flex-row max-sm:flex-col mt-20">
+        <div className="w-full sm:mb-10 md:mb-0">
+          <h1 className="text-[80px] max-md:text-6xl sm:ml-25 mt-20 font-bold text-gray-800 leading-20 max-sm:text-[60px]  ">
             Deliver Your <br />
             Delicious Food at <br />
             <span className="text-[#EA4424]">Lightning Speed</span>
           </h1>
+          <p className="hidden lg:block text-lg font-mono font-bold leading-10 ml-24 mt-5">
+            Our job is to satisfy your hunger with delicious food <br /> and fast, free delivery.
+          </p>
+          <button className="p-4 m-4 bg-[#EA4424] text-white font-bold rounded-[50px] max-sm:hidden max-md:mt-3 max-md:p-3 ml-24 py-[11]">Get Started</button>
         </div>
         {/* Right side */}
         <div className="relative w-full  flex justify-center ">
           {/* Orange Shape */}
 
-          {/* <div className="relative w-full  flex justify-center items-center">
-            <img src={HomeOrange} alt="" className="absolute w-[738px] h-[533px]" />
+          <div className="relative w-full max-md:w-full  max-sm:hidden flex justify-center items-center  ">
+            <img src={HomeOrange} alt="" className="absolute mt-30 max-sm:h-[500px] w-full h-[400px]" />
             <img
               src={homeGirlimage}
               alt="Girl Eating"
-              className="w-[794px] h-[534px] relative z-10 right-20"
+              className="w-full object-fit max-sm:h-[500px]  h-[528px] relative z-10 "
             />
-          </div> */}
+          </div>
         </div>
       </div>
 
       {/* category  */}
 
-        <p className="text-2xl font-bold">Popular catgories</p>
-      <div className=" flex flex-wrap  gap-5">
-        
+      <p className="text-2xl m-5 text-center font-bold">Popular catgories</p>
+      <div className="flex overflow-x-auto justify-center gap-5 p-4 bg-gray-100 [&::-webkit-scrollbar]:w-2
+  [&::-webkit-scrollbar-track]:bg-gray-100
+  [&::-webkit-scrollbar-thumb]:bg-gray-300
+  dark:[&::-webkit-scrollbar-track]:bg-neutral-700
+  dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
 
-        <Categorylist catgoryImage={catgoryImage} restaurantName={"Burgers & Fast food"}/>
-        <Categorylist catgoryImage={categoryImage1}    restaurantName={"Salads"}  />
 
-        <Categorylist catgoryImage={categoryImage2}   restaurantName={"Pasta & Casuals"}      />
 
-        <Categorylist  catgoryImage={categoryImage3}  restaurantName={"Pizza"}   />
-        <Categorylist  catgoryImage={categoryImage4}  restaurantName={"Breakfast"}   />
-        <Categorylist  catgoryImage={categoryImage5}   restaurantName={"Soups"} />
+        <Categorylist catgoryImage={catgoryImage} restaurantName={"Burgers & Fast food"} />
+        <Categorylist catgoryImage={categoryImage1} restaurantName={"Salads"} />
+
+        <Categorylist catgoryImage={categoryImage2} restaurantName={"Pasta & Casuals"} />
+        <Categorylist catgoryImage={categoryImage1} restaurantName={"Salads"} />
+
+        <Categorylist catgoryImage={categoryImage2} restaurantName={"Pasta & Casuals"} />
+
+        <Categorylist catgoryImage={categoryImage3} restaurantName={"Pizza"} />
+        <Categorylist catgoryImage={categoryImage4} restaurantName={"Breakfast"} />
+        <Categorylist catgoryImage={categoryImage5} restaurantName={"Soups"} />
+        <Categorylist catgoryImage={categoryImage3} restaurantName={"Pizza"} />
+        <Categorylist catgoryImage={categoryImage4} restaurantName={"Breakfast"} />
 
 
 
@@ -66,7 +80,7 @@ function Home() {
 
       <div className="overflow-x-auto whitespace-nowrap py-4 space-x-4 flex">
 
-<RestaurantCard restaurant={{ images: [categoryImage5] }} /> 
+        <RestaurantCard restaurant={{ images: [categoryImage5] }} />
       </div>
     </div>
   );
