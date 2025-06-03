649ce6dd2768aeb0fc20fa7178d708d98857a8fe
Aravind-R-Warrier <aravindrwarrier1@gmail.com>
2025-06-03 12:41:21 +0530

    --email-Name--

--- src/components/userProfile/layout/UserProfileHeader.jsx
+++ src/components/userProfile/layout/UserProfileHeader.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const UserProfileHeader = () => {
+  const user = useSelector((state) => state.auth.user);
+  console.log(user)
+
   return (
     <div className="bg-orange-600 text-white px-6 py-8">
       <div className="flex justify-between items-start">
         <div>
-          <h1 className="text-3xl font-bold mb-1">Ashutosh</h1>
-          <p className="text-teal-100 text-lg">8580426354</p>
+          <h1 className="text-3xl font-bold mb-1">{user.name}</h1>
+          {/* <p className="text-teal-100 text-lg">{user.phone}</p> */}
+          <p className="text-teal-100 text-lg">{user.email}</p>
         </div>
         <button className="border border-orange-300 bg-orange-600 hover:bg-orange-700 text-white font-medium py-2 px-6 rounded text-l tracking-wide">
           EDIT PROFILE
